9fe550c672a1db9a22569153ca4d3e6d7633f785
Icaro Asas Virtuais <39817766+asas-virtuais@users.noreply.github.com>
2024-07-25 23:38:09 -0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,18 +3,18 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'Owl by Asas Virtuais',
+  description: 'OWL by Asas Virtuais aims to provide educational access to the best AI models.',
   copyright: '',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'en-US',
 }
 
-export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const isShowPrompt = true
+export const promptTemplate = ''
 
 export const API_PREFIX = '/api'
 
-export const LOCALE_COOKIE_NAME = 'locale'
+export const LOCALE_COOKIE_NAME = 'owl.locale.cookie.name'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
